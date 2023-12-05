5d1fa0f6a02bd2c192d830706faae04b093ec8f5
zod-codes <zodworks@gmail.com>
2023-12-05 18:54:08 +0100

    new commit

--- SPACE-TOURISM-WEBSITE/src/App.jsx
+++ SPACE-TOURISM-WEBSITE/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import Navbar from "./Components/Navbar";
 
