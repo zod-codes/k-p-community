531f8fa75108ad99c9398cdbd22282f781b9fa6c
zod-codes <zodworks@gmail.com>
2023-12-05 02:54:07 +0100

    changed BrowserRouter to HashRouter

--- SPACE-TOURISM-WEBSITE/src/App.jsx
+++ SPACE-TOURISM-WEBSITE/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import Navbar from "./Components/Navbar";
 
